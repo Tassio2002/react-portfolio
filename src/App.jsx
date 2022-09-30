75bd7d984d493147f97042006402772110ac4f27
Tassio2002 <tassio.santos2002@gmail.com>
2022-09-30 19:44:30 -0300

    Finally form of contact page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss'
 import Layout from './components/Layout/Index'
 import Home from './components/Home/index'
 import About from './components/About/index'
+import Contact from './components/Contact'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
         </Route>
       </Routes>
     </>
---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
+
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm('service_e4m6w7k', 'template_k89p785', form.current, '0yT6TzD1-3wLOZraL')
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 't', 'e', '-', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Entre em contato comigo diretamente por email, ou então pelo whatapp ou linkedin.
+          </p>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input
+                    placeholder="Nome"
+                    type="text"
+                    name="name"
+                    required
+                  />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Assunto"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Mensagem"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="Enviar" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
