0cd6d36ecaeccfb46ca308321374717713151327
Tassio2002 <tassio.santos2002@gmail.com>
2022-10-04 12:00:29 -0300

    call to github api

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -6,24 +6,10 @@ import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
 import Loader from 'react-loaders'
 
-
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
-  const nameArray = [
-    'a',
-    's',
-    's',
-    'i',
-    'o',
-    ' ',
-    'S',
-    'a',
-    'n',
-    't',
-    'o',
-    's'
-  ]
+  const nameArray = ['a', 's', 's', 'i', 'o', ' ', 'S', 'a', 'n', 't', 'o', 's']
   const jobArray = [
     'd',
     'e',
@@ -41,49 +27,52 @@ const Home = () => {
     ' ',
     'w',
     'e',
-    'b'
+    'b',
   ]
 
   useEffect(() => {
     setTimeout(() => {
       return setLetterClass('text-animate-hover')
     }, 5000)
-  }, [])
+  }, [setLetterClass])
 
   return (
     <>
-<div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>O</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>s</span>
+            <span className={`${letterClass} _14`}>ou</span>
 
-          <img src={LogoTitle} alt="logo title" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={27}
-          />
-        </h1>
-        <h2>Full-stack / JavaScript / ReactJs / NodeJS / Express</h2>
-        <Link to="/contact" className="flat-button">
-          Fale comigo
-        </Link>
+            <img src={LogoTitle} alt="logo title" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={27}
+            />
+          </h1>
+          <h2>Full-stack / JavaScript / ReactJs / NodeJS / Express</h2>
+          <Link to="/contact" className="flat-button">
+            Fale comigo
+          </Link>
+        </div>
+        <div className='logo-container'>
+          <Logo idx='15'/>
+          <Logo idx='2'/>
+        </div>
+
       </div>
-      <Logo />
-    </div>
-    <Loader type='pacman' />
+      <Loader type="pacman" />
     </>
-
   )
 }
 
